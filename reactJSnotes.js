9324d73cd8fa771e3ec92952aea1897ba63e092c
catliaw <cat.liaw@gmail.com>
2017-03-07 13:08:25 -0800

    Adding notes from workshop.

---
+++ reactJSnotes.js
@@ -0,0 +1,8 @@
+// Brian "Dougie" Douglas
+// netlify - front end hosting for websites, more fully featured github pages
+// https://twitter.com/bdougieyo?lang=en
+// Slides: http://yolo.netlify.com/assets/player/keynotedhtmlplayer#1
+
+// ReactJS
+// Docs: https://facebook.github.io/react/docs/hello-world.html
+

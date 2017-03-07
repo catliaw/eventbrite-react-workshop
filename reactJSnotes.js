9a85bf3563f368e236edd88b5fb85f37d57b9eac
catliaw <cat.liaw@gmail.com>
2017-03-07 14:48:07 -0800

    Adding solution/work for remaining exercises.

--- reactJSnotes.js
+++ reactJSnotes.js
@@ -1,8 +1,22 @@
 // Brian "Dougie" Douglas
 // netlify - front end hosting for websites, more fully featured github pages
+// https://briandouglas.me/
 // https://twitter.com/bdougieyo?lang=en
 // Slides: http://yolo.netlify.com/assets/player/keynotedhtmlplayer#1
 
 // ReactJS
 // Docs: https://facebook.github.io/react/docs/hello-world.html
 
+// virtual DOM - only changes what has been changed, not the complete pages
+
+// yarnpkg.com - package manager like npm, faster, developed by group of companies such as Facebook, faster at caching and diffing changes
+
+// JSX = JavaScript XML - syntax to write HTML within your JS
+// Babel - transpiler
+
+// Components - like directives of Angular
+// all JS in JSX lives within the same Components
+// components are functions
+// all functions have a return
+// what is returned is JSX
+// 
